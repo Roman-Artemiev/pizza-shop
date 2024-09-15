f9910b141958e1013df2883eed02bfe2842220ac
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-09-15 16:21:26 +0200

    Init project, create Header & TopBar

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Container, Title } from "@/components/shared";
+import { TopBar } from "@/components/shared/top-bar";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold"/>
+      </Container>
+      
+      <TopBar />
+    </>
+  );
+}
