ca0c476b1dbcdc6db33363693adf5fd13e31906e
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-09-15 19:41:32 +0200

    Add filters for pizzas

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import { Container, Title } from "@/components/shared";
-import { TopBar } from "@/components/shared/top-bar";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -7,8 +6,28 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="All pizzas" size="lg" className="font-extrabold"/>
       </Container>
-      
+
       <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+
+          {/* Filtration */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+
+          {/* Items list */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {/* <ProductGroupList title='Pizzas' Items={[1, 2, 3, 4, 5]} />
+              <ProductGroupList title='Combo' Items={[1, 2, 3, 4, 5]} /> */}
+              Items list
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,3 +3,8 @@ export { Header } from './header';
 export { Title } from './title';
 export { Categories } from './categories';
 export { SortPopup } from './sort-popup';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export { RangeSlider } from './range-slider';
+export { TopBar } from './top-bar';
+export { CheckboxFiltersGroup } from './checkbox-filters-group';
