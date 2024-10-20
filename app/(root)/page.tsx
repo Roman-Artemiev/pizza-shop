d142771bb7444ee942e68ea1c110a6bf38ba8846
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-10-20 08:48:34 +0200

    Add pizza search by ingredients

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,20 +5,11 @@ import {
   Title,
   TopBar,
 } from "@/shared/components/shared";
-import { prisma } from "@/prisma/prisma-client";
 import { Suspense } from "react";
+import { findPizzas, GetSearchParams } from "@/shared/lib";
 
-export default async function Home() {
-  const categories = await prisma.category.findMany({
-    include: {
-      products: {
-        include: {
-          ingredients: true,
-          items: true,
-        },
-      },
-    },
-  });
+export default async function Home({ searchParams }: { searchParams: GetSearchParams }) {
+  const categories = await findPizzas(searchParams);
 
   return (
     <>
