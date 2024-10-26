7c35288322245a7885f50fe5c9cbfc5e55ae5b89
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-10-26 13:14:41 +0200

    Add google auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,16 +3,3 @@ import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-
-// {
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_ID || '',
-//       clientSecret: process.env.GITHUB_SECRET || '',
-//     }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID || '',
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-//     }),
-//   ],
-// }
